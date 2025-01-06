25c1bb0a1b13a1631f37b4d65d8120bb6de645f2
HOSEWELL <hosewellkaranja@gmail.com>
2025-01-06 20:51:51 +0300

    fix2

--- src/app/components/Hero/index.tsx
+++ src/app/components/Hero/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { FaLinkedinIn, FaGithub, FaTwitter, FaEnvelope } from "react-icons/fa";
+import { FaLinkedinIn, FaGithub, FaEnvelope } from "react-icons/fa";
 import Image from "next/image";
 
 const jobTitles = [
