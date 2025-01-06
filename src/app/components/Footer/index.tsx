f2132ef3b6c25fe87a4a3854ffd64d9c4e32d03a
HOSEWELL <hosewellkaranja@gmail.com>
2025-01-06 20:46:28 +0300

    first commit

---
+++ src/app/components/Footer/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { FaTwitter, FaGithub, FaLinkedinIn, FaEnvelope } from 'react-icons/fa';
+
+const Footer = () => {
+  return (
+    <>
+      {/* Footer Section */}
+      <footer className="bg-gray-900 text-white py-10">
+        <div className="container mx-auto text-center">
+          {/* Social Media Icons */}
+          <div className="flex justify-center space-x-6 mb-6">
+          <a
+            href="https://www.linkedin.com/in/hosewell-karanja-47750a316?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BMEYUuQFNSZmKmxNUjyvB0A%3D%3D"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-blue-500 transition duration-300"
+          >
+            <FaLinkedinIn size={24} />
+          </a>
+          <a
+            href="mailto:hosewellkaranja@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-blue-500 transition duration-300"
+          >
+            <FaEnvelope size={24} />
+          </a>
+          <a
+            href="https://github.com/HOSEWELL"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-blue-500 transition duration-300"
+          >
+            <FaGithub size={24} />
+          </a>
+          </div>
+          {/* Email Address */}
+          <p className="text-lg text-gray-300 mb-4">hosewellkaranja@gmail.com</p>
+          {/* Footer Text */}
+          <p className="text-gray-500 text-sm">
+            &copy; {new Date().getFullYear()} Personal Portfolio. All rights reserved.
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/app/components/Portfolio/index.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+import { FaExternalLinkAlt } from "react-icons/fa";
+import Image from "next/image";
+
+const Projects = () => {
+  const projectItems = [
+    {
+      title: "EcoThreads",
+      description:
+        "A platform connecting second-hand cloth sellers with recycling companies to promote sustainability.",
+      link: "https://dashboard-pi-tan-87.vercel.app/",
+      image: "/images/landingecothreads.png",
+    },
+    {
+      title: "News App",
+      description:
+        "A responsive news app built with react featuring a user-friendly interface for browsing news articles.",
+      link: "https://news-lyart-kappa.vercel.app/",
+      image: "/images/news.png",
+    },
+    {
+      title: "Fitness Tracker",
+      description:
+        "A responsive fitness tracker app built with react featuring a user-friendly interface for tracking fitness activities.",
+      link: "https://fitness-app-smoky-five.vercel.app/",
+      image: "/images/fitness.png",
+    },
+    {
+      title: "Hotel App",
+      description:
+        "A user-friendly hotel booking platform built with React, allowing users to search for rooms, check availability, and make reservations seamlessly, updated via JSON APIs.",
+      link: "https://hotel-booking-topaz-two.vercel.app/",
+      image: "/images/hotepage.png",
+    },
+    {
+      title: "Food House",
+      description:
+        "A responsive food ordering app built with Next.js, featuring a user-friendly interface for ordering food.",
+      link: "https://foodhouse-puce.vercel.app/",
+      image: "/images/foodhouse.png",
+    },
+    {
+      title: "Brickhouse",
+      description:
+        "A user-friendly app built with Next.js, offering a seamless experience for buyers and sellers of movie shows.",
+      link: "https://brick-house-ebon.vercel.app/",
+      image: "/images/brickhouse.png",
+    },
+ 
+  ];
+
+  return (
+    <section id="projects" className="py-20 bg-gray-50">
+      <div className="container mx-auto px-4">
+        <motion.h2
+          className="text-3xl font-bold text-center mb-10 text-gray-800"
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+        >
+          My Projects
+        </motion.h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
+          {projectItems.map((project, idx) => (
+            <motion.div
+              key={idx}
+              className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 transform hover:-translate-y-2 h-[400px] w-full"
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: false }}
+              transition={{ duration: 0.5, delay: idx * 0.2 }}
+            >
+              <Image
+                src={project.image}
+                alt={project.title}
+                width={600}
+                height={300}
+                className="object-cover rounded-md mb-6 h-[200px] w-full"
+              />
+              <a
+                href={project.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-2xl font-bold text-gray-800 hover:text-blue-800 transition-colors duration-300 flex items-center mb-4"
+              >
+                {project.title} <FaExternalLinkAlt className="ml-2" />
+              </a>
+              <p className="text-black-500 mb-4 text-[12px] ">{project.description}</p>
+            </motion.div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
